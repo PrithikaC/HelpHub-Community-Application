a07679539aba58264d8882e883c2b78d8228e3f5
PrithikaC <prithikachandrasekaran2004@.com>
2024-07-20 21:47:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Home from './components/Home/Home';
+import Navbar from './components/Common/Navbar';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    
+   <> 
       <Router>
+        
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Navbar/>}/>
         </Routes>
+        
       </Router>
+      
     </>
+    
   );
 }
 
---
+++ src/components/Common/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Inline CSS for white text
+const navLinkStyle = {
+  color: 'white',
+};
+
+function Navbar() {
+  return (
+    <div className="navbar-dark bg-dark shadow">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <nav className="navbar navbar-expand-lg">
+              <div className="container-fluid">
+                <Link to="/" className="navbar-brand" style={navLinkStyle}>HelpHub Community</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <Link to="/Home" className="nav-link active" style={navLinkStyle}>Home</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/login" className="nav-link active" style={navLinkStyle}>Login</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/signup" className="nav-link active" style={navLinkStyle}>Sign in</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
