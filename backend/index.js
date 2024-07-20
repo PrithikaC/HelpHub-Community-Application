37d916d88ab5c4eda07101a283bbf076e907b925
Varshini-Suresh1 <varshinisuresh611@gmail.com>
2024-07-20 19:28:16 +0530

    Backend folder is created & db.js, user.js,index.js file are created

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express=require("express");
+const app=express();
+const cors=require("cors");
+const connection= require("./config/db");
+
+//database connection
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+const port=process.env.PORT||8080;
+app.listen(port,()=>console.log(`Listening on port ${port}`))
+
+/*
+1. It loads environment variables to process.env from a .env file using dotenv.
+2. It imports the Express module.
+3. It creates an instance of an Express application, which you can then configure and start as your server.
+4. Set up middleware
+    express.json(): Middleware to parse incoming JSON requests.
+    cors(): Middleware to enable Cross-Origin Resource Sharing (CORS).
+        CORS (Cross-Origin Resource Sharing) is used to allow or restrict requested resources on a web server depending on where the HTTP request was initiated.
+5. Sets the port from an environment variable or defaults to 8080.
+6. Starts the server and listens on the specified port, logging a message when it’s running.
+*/
