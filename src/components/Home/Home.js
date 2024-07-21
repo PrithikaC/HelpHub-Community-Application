a161427719a38e3d39e883635142e71d9ca2da47
PrithikaC <prithikachandrasekaran2004@.com>
2024-07-21 10:18:10 +0530

    commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,6 +8,7 @@ function Home() {
       <section className='section'>
         <div className='container'>
           <div className='row'>
+            
             <div className='col-md-12 text-center'>
               <h3 className='main-heading'>Our Company</h3>
               <div className='underline mx-auto'></div>
