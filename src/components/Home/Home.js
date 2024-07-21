eaba8e52b6c087bdfc3678cf8a5f72934c01724f
Varshini-Suresh1 <varshinisuresh611@gmail.com>
2024-07-21 11:08:09 +0530

    Commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ function Home() {
       <Slider />
       <section className='section'>
         <div className='container'>
-          <div className='row'>
+          <div className='row m-2'>
             <div className='col-md-12 text-center'>
               <h3 className='main-heading'>Our Company</h3>
               <div className='underline mx-auto'></div>
